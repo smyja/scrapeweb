656a19f17145823edf8291ad1446df2b3e347d3c
Smyja <akposlive59@gmail.com>
2023-05-23 16:31:40 +0100

    w

--- build_and_push.sh
+++ build_and_push.sh
@@ -7,9 +7,7 @@ rm /tmp/build_args || echo OK
 env >/tmp/build_args
 echo "--build-arg \""$(cat /tmp/build_args | sed -z 's/\n/" --build-arg "/g')"IGNORE_VAR=IGNORE_VAR\"" >/tmp/build_args
 BUILD_ARGS=$(cat /tmp/build_args)
-LOGOUT="docker logout ghcr.io"
 COMMAND="docker build -t $FULL_IMAGE_NAME -t $IMAGE_NAME_WITH_REGISTRY:latest -f $DOCKERFILE $BUILD_ARGS --no-cache ."
-/bin/bash -c "$LOGOUT"
 /bin/bash -c "$COMMAND"
 docker push $IMAGE_NAME_WITH_REGISTRY:latest
 docker push $FULL_IMAGE_NAME
